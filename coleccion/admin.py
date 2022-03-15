63965731ac6372402de6f63eabceb1dbe8dbf8c6
Fernando <riccitelli.fernando@gmail.com>
2022-03-15 11:00:56 -0300

    Colecciones y Slug ajustadas

--- coleccion/admin.py
+++ coleccion/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Coleccion
 
-admin.site.register(Coleccion)
+class ColeccionAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('coleccion_nombre',)}
+    list_display = ('coleccion_nombre', 'slug')
+
+
+admin.site.register(Coleccion, ColeccionAdmin)
