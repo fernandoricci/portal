fc660b56560cbe2b6c9ae1b986be528143980683
Fernando <riccitelli.fernando@gmail.com>
2022-04-14 12:51:43 -0300

    Detalle Producto y Vista de Tienda completo

--- tienda/models.py
+++ tienda/models.py
@@ -2,6 +2,7 @@ from typing import Collection
 from django.db import models
 from django.forms import IntegerField
 from coleccion.models import Coleccion
+from django.urls import reverse
 
 class Producto(models.Model):
     producto_nombre = models.CharField(max_length=200, unique=True)
@@ -9,11 +10,14 @@ class Producto(models.Model):
     descripcion = models.TextField(max_length=500, blank=True)
     precio = models.IntegerField()
     imagenes = models.ImageField(upload_to='photos/products')
-    stock = IntegerField()
+    stock = models.IntegerField()
     is_available = models.BooleanField(default=True)
     coleccion = models.ForeignKey(Coleccion, on_delete=models.CASCADE)
     created_date = models.DateField(auto_now_add=True)
     modified_date = models.DateField(auto_now=True)
+
+    def get_url(self):
+        return reverse ('producto_detalle', args=[self.coleccion.slug, self.slug])
 
     def __str__(self):
         return self.producto_nombre
